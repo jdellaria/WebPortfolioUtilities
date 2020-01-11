47834a649be7058a9bea7fcdc7151a422731699b
Jon Dellaria <jdellaria@gmail.com>
2020-01-11 13:32:39 -0500

    add jsoup dependancies

--- src/main/java/com/mkyong/common/App.java
+++ src/main/java/com/mkyong/common/App.java
@@ -6,14 +6,49 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.mkyong.stock.bo.StockBo;
 import com.mkyong.stock.model.Stock;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+import org.jsoup.*;
+import org.jsoup.select.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 public class App
 {
     public static void main( String[] args )
     {
       System.out.println("!!!!!!!!!!!!!!!!! Entering Application for HIBERNATE Stock !!!!!!!!!!!!!!!!!!!");
-    	ApplicationContext appContext =
-    		new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
-System.out.println("!!!!!!!!!!!!!!!!! appContext!!!!!!!!!!!!!!!!! ");
+    	ApplicationContext appContext =	new ClassPathXmlApplicationContext("spring/config/BeanLocations.xml");
+      System.out.println("!!!!!!!!!!!!!!!!! appContext!!!!!!!!!!!!!!!!! ");
+
+      String HTMLSTring  = getUrlContents("https://finance.yahoo.com/quote/AIG/history?p=AIG");
+      Document doc = Jsoup.parse(HTMLSTring);
+
+      String title = doc.title();
+      System.out.printf("Title: %s%n", title);
+//    	Element link = doc.select("table").first();
+    	Element table = doc.select("table").get(0); //select the first table.
+    	Elements rows = table.select("tr");
+      for (int i = 1; i < rows.size(); i++)
+      { //first row is the col names so skip it.
+        Element row = rows.get(i);
+        Elements cols = row.select("td");
+        if (cols.size() > 3)
+        {
+          System.out.printf("Date: %s Open: %s High: %s Low: %s Close: %s AdjClose: %s %n", cols.get(0).text(), cols.get(1).text(), cols.get(2).text(), cols.get(3).text(), cols.get(4).text(), cols.get(5).text());
+        }
+      /*    	    if (cols.get(7).text().equals("down")) {
+        downServers.add(cols.get(5).text());
+      }*/
+      }
     	StockBo stockBo = (StockBo)appContext.getBean("stockBo");
 
     	/** insert **/
@@ -35,4 +70,37 @@ System.out.println("!!!!!!!!!!!!!!!!! appContext!!!!!!!!!!!!!!!!! ");
 
     	System.out.println("Done");
     }
+
+  private static String getUrlContents(String theUrl)
+  {
+    StringBuilder content = new StringBuilder();
+
+    // many of these calls can throw exceptions, so i've just
+    // wrapped them all in one try/catch statement.
+    try
+    {
+      // create a url object
+      URL url = new URL(theUrl);
+
+      // create a urlconnection object
+      URLConnection urlConnection = url.openConnection();
+
+      // wrap the urlconnection in a bufferedreader
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+
+      String line;
+
+      // read from the urlconnection via the bufferedreader
+      while ((line = bufferedReader.readLine()) != null)
+      {
+        content.append(line + "\n");
+      }
+      bufferedReader.close();
+      }
+    catch(Exception e)
+    {
+    e.printStackTrace();
+    }
+    return content.toString();
+  }
 }
